9cddfe79fb0f831785001a9b83a8b3c9c2e440bd
Michele Rossi <mikymod@gmail.com>
2020-05-18 22:07:09 +0200

    add vendor controller and routes

--- src/controllers/vendor.controller.ts
+++ src/controllers/vendor.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { Vendor } from "../models";
 
+// Create and Save a new Vendor
 function create(req: Request, res: Response) {
   // Validate request
   if (!req.body.email) {
@@ -32,4 +33,104 @@ function create(req: Request, res: Response) {
     });
 }
 
-export { create };
+// Retrieve all Vendors from database
+function findAll(req: Request, res: Response) {
+  const displayName = req.query.displayName;
+
+  Vendor.findAll({
+    where: {
+      displayName: displayName,
+    },
+  })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while retrieving vendors",
+      });
+    });
+}
+
+// Find a single vendor
+function findOne(req: Request, res: Response) {
+  const id = req.params.id;
+  Vendor.findByPk(id)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Error retrieving Dummy with id=" + id,
+      });
+    });
+}
+
+// Update a Vendor by id in the request
+function update(req: Request, res: Response) {
+  const id = req.params.id;
+
+  Vendor.update(req.body, {
+    where: { id: id },
+  })
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "Vendor was updated successfully.",
+        });
+      } else {
+        res.send({
+          message: `Cannot update Vendor with id=${id}. Maybe Vendor was not found or req.body is empty!`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Error updating Vendor with id=" + id,
+      });
+    });
+}
+
+// Delete a Vendor with the specified id in the request
+function deleteOne(req: Request, res: Response) {
+  const id = req.params.id;
+
+  Vendor.destroy({
+    where: { id: id },
+  })
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "Vendor was deleted successfully!",
+        });
+      } else {
+        res.send({
+          message: `Cannot delete Vendor with id=${id}. Maybe Vendor was not found!`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Could not delete Vendor with id=" + id,
+      });
+    });
+}
+
+// Delete all Vendors from database
+function deleteAll(req: Request, res: Response) {
+  Vendor.destroy({
+    where: {},
+    truncate: false,
+  })
+    .then((nums) => {
+      res.send({ message: `${nums} vendors were deleted successfully!` });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all vendors.",
+      });
+    });
+}
+
+export { create, findAll, findOne, update, deleteOne, deleteAll };
--- src/routes/vendor.routes.ts
+++ src/routes/vendor.routes.ts
@@ -4,7 +4,23 @@ import * as vendor from "../controllers/vendor.controller";
 function init(app: Express) {
   var router = Router();
 
+  // Create a new Vendor
   router.post("/", vendor.create);
+
+  // Retrieve all Vendors
+  router.get("/", vendor.findAll);
+
+  // Retrieve a single Vendor with id
+  router.get("/:id", vendor.findOne);
+
+  // Update a Vendor with id
+  router.put("/:id", vendor.update);
+
+  // Delete a Vendor with id
+  router.delete("/:id", vendor.deleteOne);
+
+  // Delete all Vendors
+  router.delete("/", vendor.deleteAll);
 
   app.use("/api/vendor", router);
 }
