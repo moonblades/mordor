ccd96b2d00a1cb758bd3468152e6ad21ef349d0e
Michele Rossi <mikymod@gmail.com>
2020-07-07 23:48:32 +0200

    validate parameters in user.routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,65 +1,149 @@
 import { Express, Router } from "express";
 import * as user from "../controllers/user.controller";
+import { typeValidation } from "../middlewares/validation";
+import { param, body } from "express-validator";
+import { userSchema } from "../schemas/user.schema";
+import { reservationSchema } from "../schemas/reservation.schema";
+import { businessSchema } from "../schemas/business.schema";
 
 function init(app: Express) {
   const router = Router();
 
   // Create a new user
-  router.post("/", user.create);
+  router.post(
+    "/",
+    [
+      body("email").isEmail(),
+      body("displayName").isString(),
+      body("imageUrl").isURL(),
+    ],
+    typeValidation,
+    user.create
+  );
 
   // Retrieve all users
   router.get("/", user.findAll);
 
   // Retrieve a single user with id
-  router.get("/:id", user.findOne);
+  router.get("/:id", [param("id").isNumeric()], typeValidation, user.findOne);
 
   // Update a user with id
-  router.put("/:id", user.update);
+  router.put("/:id", userSchema(), typeValidation, user.update);
 
   // Delete a user with id
-  router.delete("/:id", user.deleteOne);
+  router.delete(
+    "/:id",
+    [param("id").isNumeric()],
+    typeValidation,
+    user.deleteOne
+  );
 
   // Delete all users
   router.delete("/", user.deleteAll);
 
   // Retrieve all reservation for user
-  router.get("/:id/reservation", user.findAllReservation);
+  router.get(
+    "/:id/reservation",
+    [param("id").isNumeric()],
+    typeValidation,
+    user.findAllReservation
+  );
 
   // Retrieve a reservation for user
-  router.get("/:id/reservation/:reservationId", user.findOneReservation);
+  router.get(
+    "/:id/reservation/:reservationId",
+    [param("id").isNumeric(), param("reservationId").isNumeric()],
+    typeValidation,
+    user.findOneReservation
+  );
 
   // Create a new reservation for user
-  router.post("/:id/reservation", user.createReservation);
+  router.post(
+    "/:id/reservation",
+    reservationSchema(),
+    typeValidation,
+    user.createReservation
+  );
 
   // Update a reservation for user
-  router.put("/:id/reservation/:reservationId", user.updateReservation);
+  router.put(
+    "/:id/reservation/:reservationId",
+    reservationSchema(),
+    typeValidation,
+    user.updateReservation
+  );
 
   // Delete a reservation for user
-  router.delete("/:id/reservation/:reservationId", user.deleteOneReservation);
+  router.delete(
+    "/:id/reservation/:reservationId",
+    [param("id").isNumeric(), param("reservationId").isNumeric()],
+    typeValidation,
+    user.deleteOneReservation
+  );
 
   // Delte all reservation for user
-  router.delete("/:id/reservation", user.deleteAllReservation);
+  router.delete(
+    "/:id/reservation",
+    [param("id").isNumeric()],
+    typeValidation,
+    user.deleteAllReservation
+  );
 
   // Retrieve all business for user
-  router.get("/:id/business", user.findAllBusinesses);
+  router.get(
+    "/:id/business",
+    [param("id").isNumeric()],
+    typeValidation,
+    user.findAllBusinesses
+  );
 
   // Create business for user
-  router.post("/:id/business", user.createBusiness);
+  router.post(
+    "/:id/business",
+    businessSchema(),
+    typeValidation,
+    user.createBusiness
+  );
 
   // Update business for user
-  router.put("/:id/business/:businessId", user.updateBusiness);
+  router.put(
+    "/:id/business/:businessId",
+    businessSchema(),
+    typeValidation,
+    user.updateBusiness
+  );
 
   // Delete business for user
-  router.delete("/:id/business/:businessId", user.deleteOneBusiness);
+  router.delete(
+    "/:id/business/:businessId",
+    [param("id").isNumeric(), param("businessId").isNumeric()],
+    typeValidation,
+    user.deleteOneBusiness
+  );
 
   // Delete all businesses for user
-  router.delete("/:id/business", user.deleteAllBusinesses);
+  router.delete(
+    "/:id/business",
+    [param("id").isNumeric()],
+    typeValidation,
+    user.deleteAllBusinesses
+  );
 
   // Add favorite to business
-  router.post("/:id/favorite/:businessId", user.addFavorite);
+  router.post(
+    "/:id/favorite/:businessId",
+    [param("id").isNumeric(), param("businessId").isNumeric()],
+    typeValidation,
+    user.addFavorite
+  );
 
   // Retrieve all favorites for user
-  router.get("/:id/favorite", user.findAllFavorites);
+  router.get(
+    "/:id/favorite",
+    [param("id").isNumeric()],
+    typeValidation,
+    user.findAllFavorites
+  );
 
   app.use("/api/user", router);
 }
