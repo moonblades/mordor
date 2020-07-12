ab3f6013daec5e1518521da203e2cf661a5d6a51
Michele Rossi <mikymod@gmail.com>
2020-07-12 13:18:14 +0200

    cleanup

--- src/routes/initRoutes.ts
+++ src/routes/initRoutes.ts
@@ -1,18 +1,10 @@
 import { Express } from "express";
 import * as Business from "./business.routes";
-// import * as Product from "./product.routes";
-import * as Reservation from "./reservation.routes";
-// import * as Schedule from "./schedule.routes";
 import * as User from "./user.routes";
-// import * as Vacation from "./vacation.routes";
 
 function initRoutes(app: Express) {
   User.init(app);
   Business.init(app);
-  // Product.init(app);
-  // Vacation.init(app);
-  Reservation.init(app);
-  // Schedule.init(app);
 }
 
 export { initRoutes };
